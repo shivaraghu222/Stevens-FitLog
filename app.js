035a5ceedeb4e4130ed31d832c9a2095e7110f8a
haoy1 <haoy1@rose-hulman.edu>
2023-04-01 21:57:24 -0400

    test added to main

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 import * as users from "./data/user.js";
+import * as workout from "./data/workout.js";
+import * as fitnessProgress from "./data/fitnessProgress.js";
 async function main() {
-
-
     try {
         const first_user = await users.get("64276ac7f55567f1578959c5"); 
         console.log(first_user);
     } catch(e) {
-        console.log("Couldn't find user");
+        console.log(e);
     }
+
+    try {
+      const map = await workout.getWeights("64276ac7f55567f1578959c5");
+      console.log(map);
+  } catch(e) {
+      console.log(e);
+  }
 }
 
 main();
