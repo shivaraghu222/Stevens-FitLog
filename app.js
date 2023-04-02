afff2a1fd6b37dd141b05f329007250bc0d51d29
haoy1 <haoy1@rose-hulman.edu>
2023-04-02 19:12:27 -0400

    serving app.js on localhost:8080

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+import express from 'express';
+
 import * as users from "./data/user.js";
 import * as workout from "./data/workout.js";
 import * as fitnessProgress from "./data/fitnessProgress.js";
+
+const app = express();
+app.use(express.static('html'));
+
+app.listen(8080, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:8080');
+});
+
 async function main() {
     try {
         const first_user = await users.get("64276ac7f55567f1578959c5"); 
